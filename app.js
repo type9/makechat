1766848b41511c96154128f58a94310c620609fb
Type9 <gabespersonal@gmail.com>
2020-12-10 13:05:01 +0800

    users can create and join channels

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const server = require('http').Server(app);
 
 //Socket.io
 const io = require('socket.io')(server);
-//We'll store our online users here
 let onlineUsers = {};
+//Save the channels in this object.
+let channels = {"General" : []}
+
 io.on("connection", (socket) => {
-  // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers);
+  // Make sure to send the channels to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
 })
 
 const exphbs  = require('express-handlebars');
--- public/index.js
+++ public/index.js
@@ -76,4 +76,24 @@ $(document).ready(()=>{
             $('.users-online').append(`<p>${username}</p>`);
         }
     });
+
+    socket.on('new channel', (newChannel) => {
+        $('.channels').append(`<div class="channel">${newChannel}</div>`);
+    });
+
+    socket.on('user changed channel', (data) => {
+        $('.channel-current').addClass('channel');
+        $('.channel-current').removeClass('channel-current');
+        $(`.channel:contains('${data.channel}')`).addClass('channel-current');
+        $('.channel-current').removeClass('channel');
+        $('.message').remove();
+        data.messages.forEach((message) => {
+          $('.message-container').append(`
+            <div class="message">
+              <p class="message-user">${message.sender}: </p>
+              <p class="message-text">${message.message}</p>
+            </div>
+          `);
+        });
+      })
   })
