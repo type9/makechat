afae1c7c437768244368b5d9367c01722395a8e2
Type9 <gabespersonal@gmail.com>
2020-12-10 13:07:25 +0800

    messages to correct channel

--- public/index.js
+++ public/index.js
@@ -28,19 +28,20 @@ $(document).ready(()=>{
     });
   
     $('#send-chat-btn').click((e) => {
-      e.preventDefault();
-      // Get the message text value
-      let message = $('#chat-input').val();
-      // Make sure it's not empty
-      if(message.length > 0){
-        // Emit the message with the current user to the server
-        socket.emit('new message', {
-          sender : currentUser,
-          message : message,
-        });
-        $('#chat-input').val("");
-      }
-    });
+        e.preventDefault();
+        // Get the client's channel
+        let channel = $('.channel-current').text();
+        let message = $('#chat-input').val();
+        if(message.length > 0){
+          socket.emit('new message', {
+            sender : currentUser,
+            message : message,
+            //Send the channel over to the server
+            channel : channel
+          });
+          $('#chat-input').val("");
+        }
+      });
 
     $('#new-channel-btn').click( () => {
         let newChannel = $('#new-channel-input').val();
@@ -52,14 +53,17 @@ $(document).ready(()=>{
         }
     })
   
-    //Output the new message
     socket.on('new message', (data) => {
-        $('.message-container').append(`
-        <div class="message">
-            <p class="message-user">${data.sender}: </p>
-            <p class="message-text">${data.message}</p>
-        </div>
-        `);
+        //Only append the message if the user is currently in that channel
+        let currentChannel = $('.channel-current').text();
+        if(currentChannel == data.channel){
+          $('.message-container').append(`
+            <div class="message">
+              <p class="message-user">${data.sender}: </p>
+              <p class="message-text">${data.message}</p>
+            </div>
+          `);
+        }
     })
     
     socket.on('get online users', (onlineUsers) => {
