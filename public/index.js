c692358ebcffdf21e87fc5ce43d3223d45ec3d31
Type9 <gabespersonal@gmail.com>
2020-12-10 12:48:07 +0800

    user can login

--- public/index.js
+++ public/index.js
@@ -6,7 +6,23 @@ app.set('view engine', 'handlebars');
 //Establish your public folder
 app.use('/public', express.static('public'));
 
-$(document).ready( () => {
-    //Connect to the socket.io server
+$(document).ready(() => {
+
     const socket = io.connect();
-});
+  
+    $('#create-user-btn').click((e) => {
+      e.preventDefault();
+      let username = $('#username-input').val();
+      if(username.length > 0){
+        //Emit to the server the new user
+        socket.emit('new user', username);
+        $('.username-form').remove();
+      }
+    });
+  
+    //socket listeners
+    socket.on('new user', (username) => {
+      console.log(`✋ ${username} has joined the chat! ✋`);
+    })
+  
+  })
